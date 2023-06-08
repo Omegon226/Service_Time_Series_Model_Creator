56258e1d5f230748f075638525f322fbca31e7ae
Omegon226 <ivan_m_smirnov@mail.ru>
2023-06-08 20:31:40 +0300

    №26 feat: Frontend data import start

--- frontend/main_page.py
+++ frontend/main_page.py
@@ -1,13 +1,32 @@
 import streamlit as st
+import json
+import pandas as pd
 
-st.title("ABOBA")
+st.title("Import Data")
 
-option = st.selectbox("aboba?", ("YES", "Hmm", "No"))
-st.write("")
-x = st.slider("X", 0, 100, 20)
-y = st.slider("X", 0, 130, 10)
+df = None
 
-inputs = {"operation": option, "x": x, "y": y}
+tab_import_csv, tab_import_json, tab_import_dict, tab_data_table = \
+    st.tabs(["Import data from CSV", "Import data from JSON", "Import data from Dict", "Data Table"])
 
-if st.button("Calculate"):
-    st.subheader(f"Response from ABOBA = {str(x + y)}")
+with tab_import_csv:
+    st.header("Import your CSV file here")
+    uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
+
+with tab_import_json:
+    st.header("Import your JSON file here")
+    uploaded_file = st.file_uploader("Choose a JSON file", accept_multiple_files=False)
+
+with tab_import_dict:
+    st.header("Import your Data in Dict format here")
+    dict_data_str = st.text_input('Dict/JSON like text', '{"a": [1, 2, 3], "b": [4, 5, 6]}')
+
+    if st.button('Create DataFrame'):
+        df = pd.DataFrame(json.loads(dict_data_str))
+
+with tab_data_table:
+    if df is None:
+        st.header("No Data to print")
+    else:
+        st.header("Data table")
+        st.dataframe(df)
