821a3a7f8c3cb36ec22fac0e435417292cbecf6f
Omegon226 <ivan_m_smirnov@mail.ru>
2023-06-08 18:11:49 +0300

    №25 fix: Now you can activate service from cmd

--- app/service_global_variables/pipelines.py
+++ app/service_global_variables/pipelines.py
@@ -3,6 +3,9 @@ from app.models.scalers.min_max_scaler import MinMaxScaler
 from app.models.scalers.standart_scaler import StandardScaler
 
 from app.models.ml_models.keras_dense_model import KerasDenseModel
+from app.models.ml_models.keras_rnn_model import KerasRNNModel
+from app.models.ml_models.keras_gru_model import KerasGRUModel
+from app.models.ml_models.keras_lstm_model import KerasLSTMModel
 
 from app.models.tests.mse_test import MSETest
 from app.models.tests.rmse_test import RMSETest
@@ -17,5 +20,6 @@ pipelines: list = []
 # Здесь хранятся все доступные для использования скейлеры, модели и методы тестирования
 
 all_scalers = {"max_abs_scaler": MaxAbsScaler, "min_max_scaler": MinMaxScaler, "standard_scaler": StandardScaler}
-all_ml_models = {"keras_dense": KerasDenseModel}
+all_ml_models = {"keras_dense": KerasDenseModel, "keras_rnn": KerasRNNModel, "keras_gru": KerasGRUModel,
+                 "keras_lstm": KerasLSTMModel}
 all_tests = {"mse": MSETest, "rmse": RMSETest, "r2": R2Test, "mae": MAETest}
